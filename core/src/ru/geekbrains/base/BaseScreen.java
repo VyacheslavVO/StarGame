5260ba7fc020bd65950cbfd738ae85c43ac5e767
VyacheslavVO <vyacheslavvo@mail.ru>
2020-05-10 03:41:25 +0300

    Домашнее задание 3-го урока. Приведение к мировой коородинатной оси

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -5,110 +5,139 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.math.MatrixUtils;
+import ru.geekbrains.math.Rect;
 
 public class BaseScreen implements Screen, InputProcessor {
 
     protected SpriteBatch batch;
 
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
+    private Vector2 touch;
+
     @Override
-    // Срабатывает первый после конструктора (аналог метода Create)
     public void show() {
-        System.out.println("show");
         Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
+        screenBounds = new Rect();
+        worldBounds = new Rect();
+        glBounds = new Rect(0, 0, 1f, 1f);
+        worldToGl = new Matrix4();
+        screenToWorld = new Matrix3();
+        touch = new Vector2();
     }
 
     @Override
-    // Срабатывает 60 раз в сек. delta - отрезок времени между срабатываниями
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.3f, 0.2f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
-    // Срабатывает в начале когда устанавливается размер экрна и когда изменили размер экрана
     public void resize(int width, int height) {
-        //System.out.println("resize width = " + width + " height = " + height);
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        batch.setProjectionMatrix(worldToGl);
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+        System.out.println("worldBounds worldBounds.height = " + worldBounds.getHeight() + " worldBounds.with = " + worldBounds.getWidth());
     }
 
     @Override
-    // Срабатывает когда свернули приложение
     public void pause() {
-        //System.out.println("pause");
     }
 
     @Override
-    // Срабатывает когда развернули приложение
     public void resume() {
-        //System.out.println("resume");
     }
 
     @Override
-    // Срабатывает когда мы приложение закрываем или закрываем экран
     public void hide() {
-        //System.out.println("hide");
         dispose();
     }
 
     @Override
-    // Деструктор, очищение ресурсов памяти
     public void dispose() {
-        //System.out.println("dispose");
         batch.dispose();
     }
 
     @Override
-    // Событие нажатия клавиши
     public boolean keyDown(int keycode) {
-        //System.out.println("keyDown keycode = " + keycode);
         return false;
     }
 
     @Override
-    // Событие отпускания клавиши
     public boolean keyUp(int keycode) {
-        //System.out.println("keyUp keycode = " + keycode);
         return false;
     }
 
     @Override
-    // Событие самоого факта наличие срабатывания клавиши
     public boolean keyTyped(char character) {
-        //System.out.println("keyTyped character = " + character);
         return false;
     }
 
     @Override
-    // Событие начало клика мышкой на экране или тап пальцем на экране
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        //System.out.println("touchDown screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
-    // Событие конец клика мышкой на экране или тап пальцем на экране
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        //System.out.println("touchUp screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer, button);
+        return false;
+    }
+
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        System.out.println("touchUp touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
-    // Событие когда нажали в одной точке на экране, протащили, отпустили в другой точке
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        //System.out.println("touchDragged screenX = " + screenX + " screenY = " + screenY);
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
+
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        System.out.println("touchDragged touch.x = " + touch.x + " touch.y = " + touch.y);
         return false;
     }
 
     @Override
-    // Срабатывает при любом движении мыши
     public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
     @Override
-    // Срабатывает при прокрутке колесика мыши. amount возвращает 1 или -1, это направление прокрутки
     public boolean scrolled(int amount) {
-        //System.out.println("scrolled amount = " + amount);
         return false;
     }
 }
